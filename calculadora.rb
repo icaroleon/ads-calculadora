c86d431e91f8e3b6f6c72018f0f9083276c51734
Icaro Leon <icvieiramg@gmail.com>
2023-06-15 15:52:43 -0300

    maisc/minus ok

--- calculadora.rb
+++ calculadora.rb
@@ -6,13 +6,13 @@ class Teacher
     @contract_type = contract_type
   end
 
-  def valor_a_receber
-    case @contract_type
-    when 'CLT'
+  def income
+    case @contract_type.downcase
+    when 'clt'
       @mensal_income
-    when 'Horista'
+    when 'horista'
       @work_hours * @hour_value
-    when 'PJ'
+    when 'pj'
       @contract_value
     else
       'Regime de pagamento inválido.'
@@ -24,28 +24,29 @@ puts "Nome do professor:"
 name = gets.chomp
 puts "Qual o regime de pagamento (CLT, Horista, PJ)?"
 contract_type = gets.chomp
+contract_type_downcase = contract_type.downcase
 
-unless ['CLT', 'Horista', 'PJ'].include?(contract_type)
+unless ['clt', 'horista', 'pj'].include?(contract_type_downcase)
   puts "Regime de pagamento inválido. Favor reinserir as informações."
   return
 end
 
-teacher = Teacher.new(name, contract_type)
+teacher = Teacher.new(name, contract_type_downcase)
 
-case contract_type
-when 'CLT'
+case contract_type_downcase
+when 'clt'
   puts "Insira o salário mensal:"
   teacher.mensal_income = gets.chomp.to_f
-when 'Horista'
+when 'horista'
   puts "Insira o número de horas trabalhadas:"
   teacher.work_hours = gets.chomp.to_f
   puts "Insira o valor da hora de trabalho:"
   teacher.hour_value = gets.chomp.to_f
-when 'PJ'
+when 'pj'
   puts "Insira o valor do contrato:"
   teacher.contract_value = gets.chomp.to_f
 else
   puts "Regime de pagamento inválido."
 end
 
-puts "Conforme o cálculo realizado, o teacher #{teacher.name} vai receber #{teacher.valor_a_receber}"
+puts "Conforme o cálculo realizado, o professor #{teacher.name}, que presta serviço na modalidade #{contract_type}, vai receber R$#{teacher.income}."
